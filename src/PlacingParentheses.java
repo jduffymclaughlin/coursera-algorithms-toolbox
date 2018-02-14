910ab86787a95e670e182d6b41d5a598e3e55627
john-mclaughlin-fw <john.mclaughlin@flywire.com>
2018-02-14 09:03:41 -0500

    paren

--- src/PlacingParentheses.java
+++ src/PlacingParentheses.java
@@ -2,11 +2,20 @@ import java.util.Scanner;
 import java.util.*;
 
 public class PlacingParentheses {
-    private static long[] MinAndMax(int i, int j) {
-        long[] MinAndMax = {Long.MIN_VALUE, Long.MAX_VALUE};
+    private static long[] MinAndMax(int i, int j, long[][] m, long[][] M, char[] ops) {
+        long[] MinAndMax = {Long.MAX_VALUE, Long.MIN_VALUE};
 
-        MinAndMax[0] = 42;
-        MinAndMax[1] = 99;
+        for (int k = i; k < j-1; k++) {
+
+            long a = eval(M[i][k], M[k+1][j], ops[k]);
+            long b = eval(M[i][k], m[k+1][j], ops[k]);
+            long c = eval(m[i][k], M[k+1][j], ops[k]);
+            long d = eval(m[i][k], m[k+1][j], ops[k]);
+
+            MinAndMax[0] = Collections.min(Arrays.asList(MinAndMax[0], a, b, c, d));
+            MinAndMax[1] = Collections.max(Arrays.asList(MinAndMax[1], a, b, c, d));
+
+        }
         return MinAndMax;
     }
 
@@ -40,25 +49,27 @@ public class PlacingParentheses {
         for (int i = 0; i < n; i++) {
             min[i][i] = nums[i];
             max[i][i] = nums[i];
-            System.out.println(Arrays.toString(max[i]));
         }
 
 
         for (int s = 1; s < n - 1; s++) {
-            for (int i = 1; i < n - s; i++) {
+            for (int i = 0; i < n - s; i++) {
+
+
+
                 j = i + s;
+                minMax = MinAndMax(i, j, min, max, ops);
 
-                minMax = MinAndMax(i, j);
-
+                System.out.println(i + " " + j);
                 min[i][j] = minMax[0];
                 max[i][j] = minMax[1];
-
             }
-            System.out.println(Arrays.toString(min[s]));
         }
 
-        System.out.println(Arrays.toString(min));
-        System.out.println(Arrays.toString(max));
+        for (int i = 0; i< n; i++) {
+            System.out.println(Arrays.toString(min[i]));
+
+        }
 
         return 100;
     }
